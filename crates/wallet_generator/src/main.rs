d70a93aed35fdbaf60b6037de01217828eb40393
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-10-15 22:12:04 +0300

    feat(wallet-generator): generate multiple addresses based on call arg

--- crates/wallet_generator/src/main.rs
+++ crates/wallet_generator/src/main.rs
@@ -3,6 +3,7 @@
 use num_bigint::{BigInt, RandBigInt};
 use rand::thread_rng;
 use sha3::{Digest, Keccak256};
+use std::env;
 
 mod test;
 
@@ -34,7 +35,10 @@ fn add(p1: &Point, p2: &Point) -> Point {
     let x3 = (lambda.pow(2) - &p1.x - &p2.x) % &modulus;
     let y3 = (lambda * (&p1.x - &x3) - &p1.y) % &modulus;
 
-    return Point { x: x3, y: y3 };
+    return Point {
+        x: &x3 & &modulus,
+        y: &y3 & &modulus,
+    };
 }
 
 /// lambda = 3 * (x1^2) / 2 * y1
@@ -50,7 +54,10 @@ fn double(p: Point) -> Point {
     let x3: BigInt = (lambda.pow(2) - 2 * &p.x) % &modulus;
     let y3: BigInt = (lambda * (&p.x - &x3) - p.y) % &modulus;
 
-    return Point { x: x3, y: y3 };
+    return Point {
+        x: &x3 & &modulus,
+        y: &y3 & &modulus,
+    };
 }
 
 fn multiply_scalar(k: BigInt, p: Point) -> Point {
@@ -88,42 +95,68 @@ const G_Y: &str = "483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D
 /// which is the public key K.
 /// K = k * G
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // Try to get the first argument which is the number of addresses to generate
+    let arg = match args.get(1) {
+        Some(val) => val,
+        None => "1",
+    };
+
+    // Parse the string argument into integer
+    let no = match arg.parse::<u64>() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Unable to parse number from argument: {}", e);
+            return;
+        }
+    };
+    let no_of_addresses_to_generate = no as usize;
+
     // Define the total number of points on the curve
     let max_range = BigInt::parse_bytes(N.as_bytes(), 16).unwrap();
 
     // Create the random number generator
     let mut rng = thread_rng();
 
-    // Generate a random number k in range 1 -> number of points on the curve (N)
-    let k = rng.gen_bigint_range(&BigInt::from(1), &max_range);
+    for i in 0..no_of_addresses_to_generate {
+        // Generate a random number k in range 1 -> number of points on the curve (N)
+        let k = rng.gen_bigint_range(&BigInt::from(1), &max_range);
 
-    // Construct the generator point G
-    let g = Point {
-        x: BigInt::parse_bytes(G_X.as_bytes(), 16).unwrap(),
-        y: BigInt::parse_bytes(G_Y.as_bytes(), 16).unwrap(),
-    };
+        // Construct the generator point G
+        let g = Point {
+            x: BigInt::parse_bytes(G_X.as_bytes(), 16).unwrap(),
+            y: BigInt::parse_bytes(G_Y.as_bytes(), 16).unwrap(),
+        };
 
-    // Compute the public key by multiplying the random number k with the generator point G
-    let pub_key_point = multiply_scalar(k, g);
+        // Compute the public key by multiplying the random number k with the generator point G
+        let pub_key_point = multiply_scalar(k, g);
 
-    // Serialize public key as hexadecimal and add `0x04` prefix
-    let pub_key_x = format!("{:064x}", pub_key_point.x);
-    let pub_key_y: String = format!("{:064x}", pub_key_point.y);
+        // Serialize public key as hexadecimal
+        let pub_key_x = format!("{:06x}", pub_key_point.x);
+        let pub_key_y: String = format!("{:06x}", pub_key_point.y);
+        println!("{}{}", pub_key_x, pub_key_y);
 
-    // 04 + x-coordinate (32 bytes/64 hex) + y-coordinate (32 bytes/64 hex)
-    // println!("{:?}", format!("04{}{}", pub_key_x, pub_key_y));
+        // 04 + x-coordinate (32 bytes/64 hex) + y-coordinate (32 bytes/64 hex)
+        // println!("{:?}", format!("04{}{}", pub_key_x, pub_key_y));
 
-    let pub_key_concat = [pub_key_x, pub_key_y].concat();
+        let mut pub_key_concat = [pub_key_x, pub_key_y].concat();
 
-    // Convert the concatenated hex string to raw bytes
-    let pub_key_concat_as_bytes = hex::decode(pub_key_concat).expect("Decoding failed");
+        // If the length of the public key is odd, prepend a "0" to make it even
+        if pub_key_concat.len() % 2 != 0 {
+            pub_key_concat = format!("0{}", pub_key_concat);
+        }
 
-    // Hash the public key (X and Y coordinates concatenated) using keccak256
-    let mut hasher = Keccak256::new();
-    hasher.update(pub_key_concat_as_bytes);
-    let result = hasher.finalize();
+        // Convert the concatenated hex string to raw bytes
+        let pub_key_concat_as_bytes = hex::decode(pub_key_concat).expect("Decoding failed");
 
-    // Get the last 20 bytes and display it in hex
-    let address_bytes = &result[12..];
-    println!("{}", format!("0x{}", hex::encode(result)));
+        // Hash the public key (X and Y coordinates concatenated) using keccak256
+        let mut hasher = Keccak256::new();
+        hasher.update(pub_key_concat_as_bytes);
+        let result = hasher.finalize();
+
+        // Get the last 20 bytes and display it in hex
+        let address_bytes = &result[12..];
+        println!("{}", format!("0x{}", hex::encode(result)));
+    }
 }
