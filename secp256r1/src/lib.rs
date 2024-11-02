f748105dcfff6f4411f4cbf5375fe92b9c5e5e3f
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-11-02 21:55:13 +0200

    chore: prototype without string params

--- secp256r1/src/lib.rs
+++ secp256r1/src/lib.rs
@@ -23,16 +23,27 @@ const B: &str = "5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604
 /// [r, s] - signature
 ///
 /// P1 = (s^-1)*message)*G
-pub fn verify_signature_secp256r1(
-    pub_x: &String,
+pub fn verify_signature_secp256r1(/* pub_x: &String,
     pub_y: &String,
     message: &String,
     r: &String,
     s: &String,
-) -> bool {
-    let message_bigint: BigInt = BigInt::parse_bytes(message.as_bytes(), 16).unwrap();
-    let r_bigint: BigInt = BigInt::parse_bytes(r.as_bytes(), 10).unwrap();
-    let s_bigint: BigInt = BigInt::parse_bytes(s.as_bytes(), 10).unwrap();
+ */) -> bool {
+    let message_bigint: BigInt = BigInt::parse_bytes(
+        "815e09a2bd2fc002455e4f7e27ded6fe16b2d5fb64e794b1330baf43240426c2".as_bytes(),
+        16,
+    )
+    .unwrap();
+    let r_bigint: BigInt = BigInt::parse_bytes(
+        "68449023142751417849721717863618968034536915122161212084967858248511514843855".as_bytes(),
+        10,
+    )
+    .unwrap();
+    let s_bigint: BigInt = BigInt::parse_bytes(
+        "3488552624795641752530543084319869349275815004565195928595219571486160100262".as_bytes(),
+        10,
+    )
+    .unwrap();
 
     let modulus: BigInt = BigInt::parse_bytes(N.as_bytes(), 16).unwrap();
     let field: BigInt = BigInt::parse_bytes(P.as_bytes(), 16).unwrap();
@@ -48,8 +59,18 @@ pub fn verify_signature_secp256r1(
 
     // Construct the Q point
     let q = Point {
-        x: BigInt::parse_bytes(pub_x.as_bytes(), 10).unwrap(),
-        y: BigInt::parse_bytes(pub_y.as_bytes(), 10).unwrap(),
+        x: BigInt::parse_bytes(
+            "44287010881208015365891457934322412831709505919103389800494869821653003543448"
+                .as_bytes(),
+            10,
+        )
+        .unwrap(),
+        y: BigInt::parse_bytes(
+            "8406816689267822401861499063555392983685355867895747502381532128270957686385"
+                .as_bytes(),
+            10,
+        )
+        .unwrap(),
     };
 
     let s_inverse = modular_inverse(&s_bigint, &modulus);
